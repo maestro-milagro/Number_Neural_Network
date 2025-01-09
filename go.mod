e7dd3efd7437dbeed45f60de196c920d7ec86d3d
milagro <millagros77@gmail.com>
2025-01-09 18:09:08 +0300

    Init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module num_neural_network
+
+go 1.23.4
+
+require (
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
+	gonum.org/v1/gonum v0.15.1 // indirect
+)
---
+++ mlp.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat/distuv"
+	"math"
+)
+
+type Network struct {
+	inputs        int
+	hiddens       int
+	outputs       int
+	hiddenWeights *mat.Dense
+	outputWeights *mat.Dense
+	learningRate  float64
+}
+
+func NewNetwork(inputs, hiddens, outputs int, rate float64) *Network {
+	net := Network{
+		inputs:       inputs,
+		hiddens:      hiddens,
+		outputs:      outputs,
+		learningRate: rate,
+	}
+	net.hiddenWeights = mat.NewDense(net.hiddens, net.inputs, randomArray(net.inputs*net.hiddens, float64(net.inputs)))
+	net.outputWeights = mat.NewDense(net.outputs, net.hiddens, randomArray(net.outputs*net.hiddens, float64(net.hiddens)))
+
+	return &net
+}
+
+//
+// Helper functions to allow easier use of Gonum
+//
+
+func dot(m, n mat.Matrix) mat.Matrix {
+	r, _ := m.Dims()
+	_, c := n.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Product(m, n)
+	return o
+}
+
+func apply(fn func(i, j int, v float64) float64, m mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Apply(fn, m)
+	return o
+}
+
+func scale(s float64, m mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Scale(s, m)
+	return o
+}
+
+func multiply(m, n mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.MulElem(m, n)
+	return o
+}
+
+func add(m, n mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Add(m, n)
+	return o
+}
+
+func addScalar(i float64, m mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	a := make([]float64, r*c)
+	for x := 0; x < r*c; x++ {
+		a[x] = i
+	}
+	n := mat.NewDense(r, c, a)
+	return add(m, n)
+}
+
+func subtract(m, n mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Sub(m, n)
+	return o
+}
+
+// randomly generate a float64 array
+func randomArray(size int, v float64) (data []float64) {
+	dist := distuv.Uniform{
+		Min: -1 / math.Sqrt(v),
+		Max: 1 / math.Sqrt(v),
+	}
+
+	data = make([]float64, size)
+	for i := 0; i < size; i++ {
+		data[i] = dist.Rand()
+	}
+	return
+}
